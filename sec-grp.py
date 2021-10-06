bde45dfe24c270dfb79d0b3e2141a770f141e234
niru <you@example.com>
2021-10-06 12:21:22 -0400

    ki

--- sec-grp.py
+++ sec-grp.py
@@ -3,11 +3,12 @@ import sys
 import os
 import subprocess
 CLUS_NAME = sys.argv[1]
-createSG = ("aws ec2 create-security-group \
-           --group-name eks_rds_db_sg \
-           --description 'Allow access for RDS Database on Port 3306' \
-           --vpc-id eksctl-{}-cluster/VPC".format(CLUS_NAME))
-os.system(createSG)
+eksVPC = ("aws ec2 describe-vpcs | jq '.[][].VpcId' | tail -1 | sed 's/"//g')
+#createSG = ("aws ec2 create-security-group \
+#           --group-name eks_rds_db_sg \
+#           --description 'Allow access for RDS Database on Port 3306' \
+#           --vpc-id eksctl-{}-cluster/VPC".format(CLUS_NAME))
+#os.system(createSG)
 
 
 #eksctl-rama-prod-cluster/VPC

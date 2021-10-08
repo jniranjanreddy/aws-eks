b3a5bbb38c562bf1b6da0b89a96f278890fea2a1
niru <you@example.com>
2021-10-08 12:34:56 -0400

    ko

--- python-scripts/cr-db.py
+++ python-scripts/cr-db.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
 import sys
 import os
-ARGUMENT = sys.argv[1]
-if ARGUMENT == "apply":
-    CRDB = ("aws rds create-db-instance \
-        --db-instance-identifier usermgmtdb \
-        --db-instance-class db.t3.micro \
-        --engine mysql \
-        --master-username dbadmin \
-        --master-user-password dbpassword11 \
-	    --allocated-storage 20 \
-	    --db-subnet-group-name eks-rds-db-subnetgroup
-    os.system(CRDB)
-elif ARGUMENT == "apply":
-    CRDB = 
+import data
+#ARGUMENT = sys.argv[1]
+#if ARGUMENT == "apply":
+#    CRDB = ("aws rds create-db-instance \
+#        --db-instance-identifier usermgmtdb \
+#        --db-instance-class db.t3.micro \
+#        --engine mysql \
+#        --master-username dbadmin \
+#        --master-user-password dbpassword11 \
+#	    --allocated-storage 20 \
+#	    --db-subnet-group-name eks-rds-db-subnetgroup
+#    os.system(CRDB)
+#elif ARGUMENT == "apply":
+#    CRDB = 
+
+print("hey i am from module: {}".format(data.vpcID))
--- python-scripts/data.py
+++ python-scripts/data.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
-vpcID = ""     # EKS VPC ID vpc-*******
+vpcID = "vpc123"     # EKS VPC ID vpc-*******
 secGroup1 = "" # eks_rds_db_sg
 secGroup2 = "" # eks-rds-db-securitygroup
+secGrp = [ "rama", "Sita"]

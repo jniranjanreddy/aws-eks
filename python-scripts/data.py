3881b010f3c9073fa40e4e0e7b10dc29354b815d
niru <you@example.com>
2021-10-08 12:41:11 -0400

    ko

--- python-scripts/data.py
+++ python-scripts/data.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-vpcID = "vpc123"     # EKS VPC ID vpc-*******
+vpcID = ""     # EKS VPC ID vpc-*******
 secGroup1 = "" # eks_rds_db_sg
 secGroup2 = "" # eks-rds-db-securitygroup
-secGrp = [ "rama", "Sita"]
+eksSubnet1 = ""
+eksSubnet2 = ""
+eksSubnet3 = ""

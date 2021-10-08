f0baee0b72909d32d696703081a2ae261218a142
niru <you@example.com>
2021-10-08 06:22:03 -0400

    cf

--- sec-grp.py
+++ sec-grp.py
@@ -1,18 +1,6 @@
 #!/usr/bin/env python3
-import sys
-import os
 import subprocess
-#CLUS_NAME = sys.argv[1]
-subprocess.call(["uname", "-a"])
-
-
-#eksVPC = ("aws ec2 describe-vpcs | jq '.[][].VpcId' | tail -1 | sed 's/"//g')
-#createSG = ("aws ec2 create-security-group \
-#           --group-name eks_rds_db_sg \
-#           --description 'Allow access for RDS Database on Port 3306' \
-#           --vpc-id eksctl-{}-cluster/VPC".format(CLUS_NAME))
-#os.system(createSG)
-
-
-#eksctl-rama-prod-cluster/VPC
-#eksctl-rama-prod-cluster/VPC
+cmd="uname -a"
+sp=subprocess.Popen(cmd,shell=True/False,stdout=subprocess.PIPE,stderr=subprocess.PIPE,universal_newlines=True)
+rc=sp.wait()
+print(sp)
